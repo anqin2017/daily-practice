1bea6938822fc627d591fc179738fbee6a65c62e
AQ <aqyao.@prevt.com>
2018-03-22 19:14:49 +0800

    增加快排算法

--- sorting.cpp
+++ sorting.cpp
@@ -50,15 +50,60 @@ void FunctionBubbleSort(vector<int>& vec)
 	}
 }
 
+//øÏ≈≈
+int OnceSort(vector<int>& vec, int first, int end)
+{
+	if (vec.size() == 0)
+	{
+		cerr << "the vector has no data!" << endl;
+		return -1 ;
+	}
+	int i = first, j = end;
+	while (i < j)
+	{
+		while (i < j && vec[i] <= vec[j])
+		{
+			j--;
+		}
+		if (i < j)
+		{
+			int temp = vec[i];
+			vec[i] = vec[j];
+			vec[j] = temp;
+		}
+		while (i < j && vec[i] <= vec[j])
+		{
+			i++;
+		}
+		if (i < j)
+		{
+			int temp = vec[i];
+			vec[i] = vec[j];
+			vec[j] = temp;
+		}
+	}
+	return i;
+}
+
+void QuickSort(vector<int>& vec, int first, int end)
+{
+	int povot = OnceSort(vec, first, end);
+	QuickSort(vec, first, povot - 1);
+	QuickSort(vec, povot + 1, end);
+}
+
+
+
 int main()
 {
 	vector<int> vec_;
 	srand((unsigned)time(NULL));
-	for (int i = 0; i < 100; ++i)
+	for (int i = 0; i < 5; ++i)
 	{
 		vec_.push_back(rand() % 100);
 	}
-	FunctionBubbleSort(vec_);
+	//FunctionBubbleSort(vec_);
+	QuickSort(vec_, 0, 4);
 	TestVector(vec_);	
 	system("pause");
 	return 0;
